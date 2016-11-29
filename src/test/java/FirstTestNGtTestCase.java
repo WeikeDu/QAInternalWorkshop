d4aff81c1e3284935ac30b59236ae04939be8b52
WeikeDu <wkdu@CNwkdu.local>
2016-11-29 17:14:02 +0800

    [Weike]clear note

--- src/test/java/FirstTestNGtTestCase.java
+++ src/test/java/FirstTestNGtTestCase.java
@@ -3,9 +3,6 @@ import org.testng.annotations.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
-/**
- * Created by wkdu on 2016/11/29.
- */
 public class FirstTestNGtTestCase {
 
     @Test
